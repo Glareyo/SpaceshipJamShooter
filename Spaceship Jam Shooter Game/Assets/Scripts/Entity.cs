3e0f67af1d4a5078e0f8fd6e1a4732b0b3697946
Evy Avila <384036@colum.edu>
2024-04-11 01:39:50 -0500

    Added player simple movement

---
+++ Spaceship Jam Shooter Game/Assets/Scripts/Entity.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    //Base class for player and enemies
+
+    public float Speed;
+    public Vector2 Direction; 
+
+    public virtual void Movement()
+    {
+
+    }
+}
---
+++ Spaceship Jam Shooter Game/Assets/Scripts/Player.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : Entity
+{
+    private PlayerControls controls;
+    private InputAction Move;
+
+
+    private void Awake()
+    {
+        controls = new PlayerControls();
+
+        controls.Enable(); //enable here for using an event
+        controls.Player.Shoot.performed += Shoot; //subscribe
+    }
+
+    private void OnEnable()
+    {
+        Move = controls.Player.MoveDirection;
+        Move.Enable();
+    }
+
+    private void OnDisable()
+    {
+        Move.Disable();
+        controls.Player.Shoot.performed -= Shoot; //unsubscribe
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        this.Movement();
+    }
+
+    //For the player to move around
+    public override void Movement()
+    {
+        var move = Move.ReadValue<Vector2>();
+        Direction.x = move.x; 
+        Direction.y = move.y;
+
+        transform.Translate(Direction * Speed * Time.deltaTime);
+    }
+
+    //an event for the ship to shoot once 
+    //TODO: have another class to handle
+    private void Shoot(InputAction.CallbackContext callback)
+    {
+        Debug.Log("Shooting");
+    }
+}
