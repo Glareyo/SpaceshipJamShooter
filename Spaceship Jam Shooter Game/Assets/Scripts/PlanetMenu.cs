4c09e9731974149eb84bf05b64e9f196b66e71cd
Evy Avila <384036@colum.edu>
2024-05-01 23:44:02 -0500

    hover over planet change image sprite

--- Spaceship Jam Shooter Game/Assets/Scripts/PlanetMenu.cs
+++ Spaceship Jam Shooter Game/Assets/Scripts/PlanetMenu.cs
@@ -1,16 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class PlanetMenu : MonoBehaviour
+public class PlanetMenu : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField]
     private int GoToSceneID;
 
+    private Sprite OriginalImage;
+
+    [SerializeField]
+    private Sprite HoverImage;
+
+    Image image;
+
+    private void Start()
+    {
+        image = GetComponent<Image>();
+        OriginalImage = image.sprite;
+        
+    }
+
     public void SelectPlanet()
     {
-        //Debug.Log("Planet selected");
         SceneManager.LoadScene(GoToSceneID);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        image.sprite = HoverImage;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        image.sprite = OriginalImage;
     }
 }
